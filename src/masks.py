012f7cd52cd60e0e3b89dcbb45d9880a16bf29f0
drauzer89 <drauzer89@gmail.com>
2024-09-18 21:59:07 +0300

    init

--- src/masks.py
+++ src/masks.py
@@ -4,19 +4,14 @@ def get_mask_card_number(card_number: int) -> str:
     символы указанные в hidden_chars = [6, 7, 8, 9, 10, 11]
     """
     hidden_chars = [6, 7, 8, 9, 10, 11]
-    hidden_number = ''
+    hidden_number = ""
     for i, char in enumerate(str(card_number)):
         if i in hidden_chars:
-            hidden_number += '*'
+            hidden_number += "*"
         else:
             hidden_number += char
-    blocks = [
-        hidden_number[0:4],
-        hidden_number[4:8],
-        hidden_number[8:12],
-        hidden_number[12:16]
-    ]
-    return ' '.join(blocks)
+    blocks = [hidden_number[0:4], hidden_number[4:8], hidden_number[8:12], hidden_number[12:16]]
+    return " ".join(blocks)
 
 
 def get_mask_account(account_number: int) -> str:
@@ -25,35 +20,5 @@ def get_mask_account(account_number: int) -> str:
     символы указанные в hidden_chars = [0, 1]
     """
     account_number_txt = str(account_number)
-    hidden_number = '**' + account_number_txt[-4:]
-    return  hidden_number
-
-
-def mask_account_card(cards_accounts):
-    numbers = ''
-    mask_cards_accounts = ''
-    for char in cards_accounts:
-        if char.isdigit():
-            numbers += char
-    if len(numbers) > 16:
-        mask_cards_accounts = get_mask_account(numbers)
-    else:
-        mask_cards_accounts = get_mask_card_number(numbers)
-    return mask_cards_accounts
-
-print(mask_account_card('Счет 73654108430135874305'))
-print(mask_account_card("Maestro 7000792289606361"))
-
-import datetime
-
-def get_date(input_string):
-    try:
-        date_object = datetime.datetime.strptime(input_string, '%d.%m.%Y')
-        return date_object.strftime('%d.%m.%Y')
-    except ValueError:
-        return "Некорректный формат даты"
-
-
-# Пример использования
-result = get_date("11.07.2018")
-print(result)
+    hidden_number = "**" + account_number_txt[-4:]
+    return hidden_number
---
+++ src/widget.py
@@ -0,0 +1,49 @@
+import datetime
+import masks
+
+
+def mask_account_card(cards_accounts: str) -> str:
+    """
+    Принимает строковое значение забирает из их только цыфры
+    по количеству цифр сооотносит номер карты или счета после
+    проводит через функции get_mask_account либо get_mask_card_number
+    """
+    numbers = ""
+    mask_cards_accounts = ""
+    for char in cards_accounts:
+        if char.isdigit():
+            numbers += char
+        else:
+            mask_cards_accounts += char
+    if len(numbers) > 16:
+        mask_cards_accounts += masks.get_mask_account(numbers)
+
+    else:
+        mask_cards_accounts += masks.get_mask_card_number(numbers)
+    return mask_cards_accounts
+
+
+def get_date(data_st: str) -> str:
+    """
+    Принимает на вход строку 2024-03-11T02:26:18.671407 и отдает корректный результат в формате ДД.ММ.ГГГГ
+    В случаи некоректного вода возращает:
+    "Некорректный формат даты, укажите дату в формате день месяц год (ДД.ММ.ГГГГ)", "Cегодня", ДД.ММ.ГГГГ)
+    """
+    try:
+        date_object = datetime.datetime.strptime(data_st, "%Y-%m-%dT%H:%M:%S.%f")
+        return date_object.strftime("%d.%m.%Y")
+    except ValueError:
+        return "Некорректный формат даты, укажите дату в формате 2024-03-11T02:26:18.671407"
+
+
+print(get_date("2024-11-11T02:26:18.671407"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Maestro 1596837868705199"))
+print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Счет 35383033474447895560"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(mask_account_card("Visa Platinum 8990922113665229"))
+print(mask_account_card("Visa Gold 5999414228426353"))
+print(mask_account_card("Счет 73654108430135874305"))
