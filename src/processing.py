0e5ed78910599709cfa686ba93301a376fd762f8
KotShredengera <drauzer89@gmail.com>
2024-09-26 01:05:20 +0300

    Добавлено описания к функциям модуля processing  1. filter_by_state      Принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED'). Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует указанному значению.
     2. sort_by_date
        Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание). Функция возвращает новый список, отсортированный по дате (date).

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(list_operations, operation_status="EXECUTED"):
+def filter_by_state(list_operations: list[dict], operation_status: str = "EXECUTED") -> list[dict]:
     """
     Принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
     Функция возвращает новый список словарей, содержащий только те словари,
@@ -6,33 +6,32 @@ def filter_by_state(list_operations, operation_status="EXECUTED"):
     """
     try:
         list_operations_state = []
-        key = {}
         for operations in list_operations:
             if operations["state"] in operation_status:
                 list_operations_state.append(operations)
         return list_operations_state
 
     except Exception:
-        return
+        return "ошибка данных"
 
 
-def sort_by_date(list_operations, ascending=False):
+def sort_by_date(list_operations: list[dict], ascending: bool = True) -> list[dict]:
     """
     Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
     Функция возвращает новый список, отсортированный по дате (date).
     """
 
-    sort_by_date = sorted(list_operations, key=lambda x: x.get("date", 0), reverse=ascending)
+    sort_by_date = sorted(list_operations, key=lambda x: x.get("date", 0), reverse=not ascending)
     return sort_by_date
 
 
-list = [
+list_pr = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
-print(sort_by_date(list))
-print(filter_by_state(list))
-print(filter_by_state(list, "CANCELED"))
+print(sort_by_date(list_pr))
+print(filter_by_state(list_pr))
+print(filter_by_state(list_pr, "CANCELED"))
