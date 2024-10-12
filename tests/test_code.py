9fb72fb63deb1b291b9c4ff12823ba879da79434
drauzer89 <drauzer89@gmail.com>
2024-10-12 23:12:43 +0300

    init

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,4 +1,5 @@
 import pytest
+
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import mask_account_card, get_date
 from src.processing import filter_by_state, sort_by_date
