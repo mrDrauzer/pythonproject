8d441d83e51b9a8859214928860c09aee52f7638
KotShredengera <drauzer89@gmail.com>
2024-11-18 14:51:44 +0300

    ADD logerr

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,22 @@
 import json
 import os
+import logging
 from json import JSONDecodeError
+from venv import logger
+
+logger = logging.getLogger('open_json')
+logger.setLevel(logging.INFO)
+file_handle = logging.FileHandler('logs/open_json.log')
+file_formatter = logging.Formatter('%(asctime)s- #(name)s - %(levelname)s: %(message)s')
+file_handle.setFormatter(file_formatter)
+logger.addHandler(file_handle)
+
 
 
 def open_json(local_json_file=r"c:\python\project\home_work\data\operations.json"):
     """Принимат на вход сслыку на файл JSON выдает список словарей"""
     try:
+        logerr.info(f'Выполнение чтения JSON')
         if not os.path.exists(local_json_file):
             print(f"Файл {local_json_file} не найден")
             return "[]"
