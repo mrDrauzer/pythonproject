89074e93ce72897fa4f4ea104465086051e9f012
KotShredengera <drauzer89@gmail.com>
2024-11-21 16:10:23 +0300

    ADD loger  to utils and masks

--- src/utils.py
+++ src/utils.py
@@ -4,18 +4,20 @@ import logging
 from json import JSONDecodeError
 
 
-logger = logging.getLogger('open_json')
+logger = logging.getLogger("open_json")
 logger.setLevel(logging.DEBUG)
-file_handle = logging.FileHandler(os.path.join(os.path.dirname(__file__), "..", "logs", "open_json.log"), encoding='utf-8')
-file_formatter = logging.Formatter('%(asctime)s- #(name)s - %(levelname)s: %(message)s')
+file_handle = logging.FileHandler(
+    os.path.join(os.path.dirname(__file__), "..", "logs", "open_json.log"), encoding="utf-8"
+)
+file_formatter = logging.Formatter("%(asctime)s- #(name)s - %(levelname)s: %(message)s")
 file_handle.setFormatter(file_formatter)
 logger.addHandler(file_handle)
 
 
-def open_json(local_json_file=r"c:\python\project\home_work\data\operations.json"):
+def open_json(local_json_file=os.path.join(os.path.dirname(__file__), "..", "data", "operations.json")):
     """Принимат на вход сслыку на файл JSON выдает список словарей"""
     try:
-        logger.debug(f'Выполнение чтения JSON')
+        logger.debug(f"Выполнение чтения JSON")
         if not os.path.exists(local_json_file):
             print(f"Файл {local_json_file} не найден")
             return "[]"
@@ -24,7 +26,7 @@ def open_json(local_json_file=r"c:\python\project\home_work\data\operations.json
                 data = json.load(f)
                 return data
     except JSONDecodeError:
-        logger.error((f'Произошла ошибка'))
+        logger.error((f"Произошла ошибка"))
         print("Фаил пустой")
         return "[]"
 
@@ -33,7 +35,7 @@ def open_json(local_json_file=r"c:\python\project\home_work\data\operations.json
 # current_dir = os.path.dirname(__file__)  # Текущий каталог скрипта
 # json_file = os.path.join(current_dir, "data", "operations.json")  # Относительный путь к файлу
 
-#print(open_json(r'c:\python\project\home_work\data\operations.jso'))
+# print(open_json(r'c:\python\project\home_work\data\operations.jso'))
 
 # current_dir = os.path.dirname(__file__),   # Текущий каталог скрипта
 
@@ -45,3 +47,9 @@ print(open_json(os.path.join(os.path.dirname(__file__), "..", "data", "empti.jso
 
 # local_json_file = os.path.join(os.path.dirname(__file__), "..", "data", "operations.json")
 # if not os.path.exists(local_json_file):
+
+if __name__ == "__main__":
+    card_number = 1234567890
+    account_number = 9876543210
+    print(open_json(2))
+    print(open_json(2))
